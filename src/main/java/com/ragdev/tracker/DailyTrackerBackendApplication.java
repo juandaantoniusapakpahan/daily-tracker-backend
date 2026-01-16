1c5ad3ec3ac438765b7d5ef83daf674c88408325
Juanda Antonius Pakpahan <juandaantoniuspakpahan@Zodiacs-MacBook-Pro.local>
2026-01-16 09:54:58 +0700

    init: spring boot daily tracker backend

---
+++ src/main/java/com/ragdev/tracker/DailyTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ragdev.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyTrackerBackendApplication.class, args);
+	}
+
+}
