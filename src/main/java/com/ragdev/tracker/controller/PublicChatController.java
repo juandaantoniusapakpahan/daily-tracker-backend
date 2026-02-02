b81b76fbfd1a6770e60bd07b4ac22e02eeec5acd
juandaantoniuspakpahan <juanda.pakpahan@phincon.com>
2026-02-02 11:59:06 +0700

    update import package

--- src/main/java/com/ragdev/tracker/controller/PublicChatController.java
+++ src/main/java/com/ragdev/tracker/controller/PublicChatController.java
@@ -8,6 +8,8 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
+
 @Controller
 @RequiredArgsConstructor
 public class PublicChatController {
@@ -17,22 +19,18 @@ public class PublicChatController {
     @MessageMapping("/chat.send")
     public void sendMessage(
             @Payload ReqChatMessageDto request,
-            java.security.Principal principal) { // Gunakan Principal, bukan UserDetailsImpl langsung
+            Principal principal) {
 
         if (principal == null) {
             throw new RuntimeException("User Unauthorized");
         }
 
-        // Ambil username dari principal
         String username = principal.getName();
 
         ResChatMessageDto res = new ResChatMessageDto();
         res.setUsername(username);
         res.setContent(request.getContent());
         res.setTimestamp(java.time.LocalDateTime.now());
-
-        // Note: 'mine' akan kita tentukan di Frontend agar lebih akurat
-        // karena broadcast dikirim ke semua orang termasuk pengirim.
 
         messagingTemplate.convertAndSend("/topic/public", res);
     }
