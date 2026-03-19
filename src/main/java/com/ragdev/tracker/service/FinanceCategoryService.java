7bfbbcd8977f225df68939fbbf72de4e453c2703
Juanda Antonius Pakpahan <juandaantoniuspakpahan@Zodiacs-MacBook-Pro.local>
2026-03-19 16:42:31 +0700

    remove redis

--- src/main/java/com/ragdev/tracker/service/FinanceCategoryService.java
+++ src/main/java/com/ragdev/tracker/service/FinanceCategoryService.java
@@ -1,8 +1,5 @@
 package com.ragdev.tracker.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ragdev.tracker.dto.ReqFCategoryDto;
 import com.ragdev.tracker.dto.ResFCategoryDto;
 import com.ragdev.tracker.entity.FinanceCategory;
@@ -10,83 +7,60 @@ import com.ragdev.tracker.entity.User;
 import com.ragdev.tracker.exception.ResourceNotFoundException;
 import com.ragdev.tracker.mapper.FCategoryMapper;
 import com.ragdev.tracker.repository.FinanceCategoryRepository;
-import com.ragdev.tracker.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FinanceCategoryService {
 
     private final FinanceCategoryRepository fCategoryRepo;
-    private final RedisService redisService;
-    private String getAllKey = "Cate:GetAll";
 
-    public FinanceCategoryService(FinanceCategoryRepository fCategoryRepo,
-                                  RedisService redisService) {
-
+    public FinanceCategoryService(FinanceCategoryRepository fCategoryRepo) {
         this.fCategoryRepo = fCategoryRepo;
-        this.redisService = redisService;
     }
 
     public ResFCategoryDto create(User user, ReqFCategoryDto dto) {
-        String key = getAllKey+user.getId();
         FinanceCategory fCategory = FCategoryMapper.toEntity(dto);
         fCategory.setUser(user);
         fCategoryRepo.save(fCategory);
-        redisService.removeDataRedis(key);
         return FCategoryMapper.toDto(fCategory);
     }
 
     @Transactional
     public ResFCategoryDto update(Long userId, ReqFCategoryDto dto) {
-        String key = getAllKey+userId;
         FinanceCategory fCategory = getById(dto.getId());
         fCategory.setName(dto.getName());
         fCategoryRepo.save(fCategory);
-        redisService.removeDataRedis(key);
         return FCategoryMapper.toDto(fCategory);
     }
 
     @Transactional
-    public ResFCategoryDto inActive(Long userId,Long id) {
-        String key = getAllKey+userId;
+    public ResFCategoryDto inActive(Long userId, Long id) {
         FinanceCategory fCategory = getById(id);
         fCategory.setActive(false);
         fCategoryRepo.save(fCategory);
-        redisService.removeDataRedis(key);
         return FCategoryMapper.toDto(fCategory);
     }
 
     @Transactional
     public ResFCategoryDto active(Long userId, Long id) {
-        String key = getAllKey+userId;
         FinanceCategory fCategory = getById(id);
         fCategory.setActive(true);
         fCategoryRepo.save(fCategory);
-        redisService.removeDataRedis(key);
         return FCategoryMapper.toDto(fCategory);
     }
 
     public FinanceCategory getById(Long id) {
-       return fCategoryRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Category not found"));
+        return fCategoryRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
     }
 
-    public List<ResFCategoryDto> getAll(Long userId)  {
-        String key = getAllKey + userId;
-        List<ResFCategoryDto> redisData = redisService.getValueList(key, ResFCategoryDto.class);
-
-        if(redisData == null){
-            redisData = fCategoryRepo.findByUserId(userId)
-                    .stream()
-                    .map(FCategoryMapper::toDto)
-                    .toList();
-            redisService.saveToRedis(key, redisData);
-        }
-        return redisData;
+    public List<ResFCategoryDto> getAll(Long userId) {
+        return fCategoryRepo.findByUserId(userId)
+                .stream()
+                .map(FCategoryMapper::toDto)
+                .toList();
     }
 }
